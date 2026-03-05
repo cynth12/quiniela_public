40da2500351d32411f0a2403c97b1b15560688fb
cynth12 <gonzalez.cynth12@gmail.com>
2026-03-05 14:57:30 -0500

    log

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,10 +14,18 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
     'webhook/mp',
     '/webhook/mp',
+     'https://quinielazas.com/webhook/mp',
 ];
     protected function tokensMatch($request)
     {
         \Log::info('VerifyCsrfToken ejecutado, except: ', $this->except);
         return parent::tokensMatch($request);
     }
+
+    public function handle($request, \Closure $next)
+{
+    \Log::info('CSRF middleware ejecutado', ['uri' => $request->path()]);
+    return parent::handle($request, $next);
+}
+
 }
