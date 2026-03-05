e13870e2236c32f4474b8e5d151d0997f97e7608
cynth12 <gonzalez.cynth12@gmail.com>
2026-03-05 14:10:43 -0500

    cambio a api otra vez

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\QuinielaPublicController;
+use App\Http\Controllers\PagoController;
+
 
 
 Route::post('pagos/webhook', [PagoController::class, 'webhook'])->name('pagos.webhook');
