ddb0b1c46bb4b0244f7eee5b5fe28955af432260
cynth12 <gonzalez.cynth12@gmail.com>
2026-04-22 10:26:58 -0500

    cerrar

--- resources/views/jornada/show.blade.php
+++ resources/views/jornada/show.blade.php
@@ -50,8 +50,16 @@
                             @endforeach
                         </tbody>
                     </table>
-                    <button type="submit" class="btn btn-warning mt-3">📅 Cerrar jornada y guardar resultados</button>
-                    <form action="{{ route('jornada.cerrar.simple', $jornada->id) }}" method="POST" style="display:inline;">
+                    <form action="{{ route('jornada.cerrar', $jornada->id) }}" method="POST" style="display:inline;">
+                        @csrf
+                        <button type="submit" class="btn btn-warning mt-3">
+                            📅 Cerrar jornada y guardar resultados
+                        </button>
+                    </form>
+
+                    {{-- Botón rojo: cerrar jornada simple (sin validar resultados) --}}
+                    <form action="{{ route('jornada.cerrar.simple', $jornada->id) }}" method="POST"
+                        style="display:inline;">
                         @csrf
                         <button type="submit" class="btn btn-danger">
                             🔒 Cerrar jornada
